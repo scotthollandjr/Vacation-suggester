e532ee6e15b82ef9a29d061e7a2725c891db9fb2
Scott Holland <scotthollandjr@gmail.com>
2016-01-12 12:54:16 -0800

    Add class selector toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  
+  $("form#questions").submit(function() {
+    var weatherVal = $("select#weather").val();
+    var landscapeVal = $("select#landscape").val();
+    var fancyVal = $("select#fancy").val();
+    var distanceVal = $("select#distance").val();
+    var populationVal = $("select#population").val();
+
+    var className = "." + weatherVal + "." + landscapeVal + "." + fancyVal + "." + distanceVal + "." + populationVal;
+
+    $("#first").toggle();
+    $(className).toggle();
+
+    event.preventDefault();
+  });
 });
