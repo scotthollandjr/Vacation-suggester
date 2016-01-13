0d19c41c5747c8261255ee09185aeb5a9b96b439
Scott Holland <scotthollandjr@gmail.com>
2016-01-12 17:16:42 -0800

    Add finishing edits to vacation results

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,23 @@ $(document).ready(function() {
     var weatherVal = $("select#weather").val();
     var landscapeVal = $("select#landscape").val();
     var fancyVal = $("select#fancy").val();
-    var distanceVal = $("select#distance").val();
-    var populationVal = $("select#population").val();
 
-    var className = "." + weatherVal + "." + landscapeVal + "." + fancyVal + "." + distanceVal + "." + populationVal;
+    var distanceVal = $("select#distance").val();
+    var factionVal = $("select#faction").val();
+
+    var className = "." + weatherVal + "." + landscapeVal + "." + fancyVal;
+
+    if ( distanceVal === "far" ) {
+      $("p.distance").append("<br><strong>Good thing you want to travel far, cuz this place is off the maps!</strong>");
+    } else if ( distanceVal === "notfar" ){
+      $("p.distance").append("<br><strong>You said not too far, this planet isn't TOO far..</strong>");
+    }
+
+    if ( factionVal === "dark" ) {
+      $("#red").toggle();
+    } else if ( factionVal === "light" ) {
+      $("#green").toggle();
+    }
 
     $("#first").toggle();
     $(className).toggle();
